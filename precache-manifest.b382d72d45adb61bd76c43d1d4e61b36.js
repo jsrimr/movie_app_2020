3a84d8aa1d96befd989314956c55d08b951cffde
jsrimr <jsrimr@naver.com>
2020-04-15 21:49:54 +0900

    Updates

---
+++ precache-manifest.b382d72d45adb61bd76c43d1d4e61b36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea19daf2f9ba9b8ca8e99d63abfcaef",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "5277783e2acc1a75c243",
+    "url": "/movie_app_2020/static/css/main.f42403a8.chunk.css"
+  },
+  {
+    "revision": "bf32d5eca7f9bedd703e",
+    "url": "/movie_app_2020/static/js/2.c4a6958a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.c4a6958a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5277783e2acc1a75c243",
+    "url": "/movie_app_2020/static/js/main.6e5e4b9b.chunk.js"
+  },
+  {
+    "revision": "6ae996d59509b91f0acc",
+    "url": "/movie_app_2020/static/js/runtime-main.d8dd5193.js"
+  }
+]);
